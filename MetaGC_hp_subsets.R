c6808260d2a5d43fcdb5a56b1897d6527cb2d1e9
Joe <joerothwell1@gmail.com>
2023-09-01 16:45:52 +0200

    Updates to subgroup analyses

--- MetaGC_hp_subsets.R
+++ MetaGC_hp_subsets.R
@@ -1,16 +1,15 @@
-# Read in participant data and pos and neg data
-dat <- read.csv("participant_data1.csv")
-pos <- read.csv("metabolomics_pos.csv") %>% select(-(Idepic_Bio:Cncr_Caco_Stom))
-neg <- read.csv("metabolomics_neg.csv") %>% select(-(Idepic_Bio:Center)) %>% select(-Match_Caseset, -Cncr_Caco_Stom, -X)
-posneg <- bind_cols(pos, neg) %>% rename(feat.no = X)
+# Read in participant data and pos and neg data and join
+dat <- read_sas("meta_gc_caco.sas7bdat")
+pos <- read_sas("meta_gc_untg_rp_pos_metabo.sas7bdat") %>% select(-(Idepic_Bio:Cncr_Caco_Stom))
+neg <- read_sas("meta_gc_untg_rp_neg_metabo.sas7bdat") %>%
+  select(-(Idepic_Bio:Center)) %>% select(-Match_Caseset, -Cncr_Caco_Stom)
 
-# Pos mode ----
+posneg <- bind_cols(pos, neg)
+
+# Both modes ----
 # Join participant data to metabolomics data
 library(tidyverse)
 posdat <- right_join(dat, posneg, by = "Idepic")
-
-posdat %>% group_by(Match_Caseset) %>% filter(any(HPPOS == 1)) %>% n()
-
 
 # Keep CC pairs with no HPPOS missings
 nonmiss.hp <- posdat %>% group_by(Match_Caseset) %>% filter(!anyNA(HPPOS)) %>% ungroup() # 400
@@ -30,7 +29,6 @@ discordant <- discordant %>% mutate(disc.type = ifelse(HPPOS == 1 & Cncr_Caco_St
 discordant10 <- discordant %>% group_by(Match_Caseset) %>% filter(sum(disc.type) == 1) %>% ungroup() # 134/2 = 67
 discordant01 <- discordant %>% group_by(Match_Caseset) %>% filter(sum(disc.type) == 0) %>% ungroup() # 34/2 = 17
 
-
 # Identify CC pairs with at least one missing
 anymiss.hp <- posdat %>% group_by(Match_Caseset) %>% filter(is.na(sum(HPPOS))) %>% ungroup() # 474/2 = 237
 
@@ -38,14 +36,14 @@ anymiss.hp <- posdat %>% group_by(Match_Caseset) %>% filter(is.na(sum(HPPOS))) %
 allmiss.hp <- posdat %>% group_by(Match_Caseset) %>% filter(sum(is.na(HPPOS))==2) %>% ungroup() # 460/2 = 230
 
 # Identify CC pairs with an HPPOS == 1 and a missing
-miss.hp1 <- miss.hp %>% group_by(Match_Caseset) %>% filter(sum(HPPOS, na.rm = TRUE) == 1) %>% ungroup() # 10/2 = 5
-table(HP = miss.hp1$HPPOS, case = miss.hp1$Cncr_Caco_Stom.x)
+miss.hp1 <- anymiss.hp %>% group_by(Match_Caseset) %>% filter(sum(HPPOS, na.rm = TRUE) == 1) %>% ungroup() # 10/2 = 5
+table(HP = miss.hp1$HPPOS, case = miss.hp1$Cncr_Caco_Stom)
 # 5 pairs have HPPOS either in the case or the control, of which 4 pairs have HPPOS for the case
 
 # Make new variable to distinguish 2 discordance types
-miss.hp1 <- miss.hp1 %>% mutate(disc.type = ifelse(HPPOS == 1 & Cncr_Caco_Stom.x == 1, 1, 0))
+miss.hp1 <- miss.hp1 %>% mutate(disc.type = ifelse(HPPOS == 1 & Cncr_Caco_Stom == 1, 1, 0))
 
 # Get discordance types
-miss.disc10 <- miss.hp1 %>% group_by(Match_Caseset.x) %>% filter(sum(disc.type, na.rm = TRUE) == 1) %>% ungroup() # 8/2 = 4
-miss.disc01 <- miss.hp1 %>% group_by(Match_Caseset.x) %>% filter(sum(disc.type, na.rm = TRUE) == 0) %>% ungroup() # 1/2 = 1
+miss.disc10 <- miss.hp1 %>% group_by(Match_Caseset) %>% filter(sum(disc.type, na.rm = TRUE) == 1) %>% ungroup() # 8/2 = 4
+miss.disc01 <- miss.hp1 %>% group_by(Match_Caseset) %>% filter(sum(disc.type, na.rm = TRUE) == 0) %>% ungroup() # 1/2 = 1
 
--- Meta_GC_sensitivity.R
+++ Meta_GC_sensitivity.R
@@ -5,28 +5,35 @@
 # Subset metabolomics data only using prefix. Log transform data.
 # Model with essential covariates
 library(tidyverse)
-posints <- concordant11 %>% select(starts_with("Untg_Rp_Pos")) %>% as.matrix() %>% log2()
+ints <- concordant11 %>% select(starts_with("Untg_Rp")) %>% as.matrix() %>% log2()
 
-clr.adj <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + QE_ENERGY + QE_ALC + 
+# Get feature names binding together mode and m/z from data frame labels
+mode <- c(rep("pos", ncol(pos)), rep("neg", 2689-ncol(pos)))
+features <- sapply(posneg[ , 1:ncol(ints)], attr, "label") %>% unname
+featnames <- paste(mode, features, sep = "_")
+
+clr <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + QE_ENERGY + QE_ALC + 
                                 L_School + Fasting_C + strata(Match_Caseset), data = concordant11)
 
 library(survival)
 varlist <- c("Smoke_Stat", "Alc_Drinker", "Center", "L_School", "Fasting_C")
 concordant11 <- concordant11 %>% mutate(across((varlist), as.factor))
 # Apply model across subset
-mod.adj2 <- apply(posints, 2, clr.adj)
-
-features <- colnames(posints)
+mod.adj2 <- apply(ints, 2, clr)
 
 library(broom)
 res.adj2 <- map_df(mod.adj2, tidy, exponentiate = T, conf.int = T) %>% 
   filter(term == "x") %>% mutate(p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
   select(feature, everything(), -term) %>% mutate(feat.no = 1:length(mod.adj2))
 
+# Format OR and CI
+tab.adj <- res.adj2 %>%
+  select(feature, estimate, conf.low, conf.high, everything()) %>%
+  mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("CI95", B1, conf.low, hyph, conf.high, B2, sep = "")
+
 # No significant features after p-value adjustment
-
-
-
 
 # (b)	run sensitivity analyses on the n=105 + n=71 (i.e., n=176) case-control pairs where the case is Hppos=1, 
 # but the control could be 1 or 0.
@@ -34,41 +41,51 @@ res.adj2 <- map_df(mod.adj2, tidy, exponentiate = T, conf.int = T) %>%
 # This is concordant11 + discordant10 (168 + 134 obs = actually 151 pairs)
 discordant10 <- discordant10 %>% mutate(across((varlist), as.factor)) %>% select(-disc.type)
 caseposall <- bind_rows(concordant11, discordant10)
-posints <- caseposall %>% select(starts_with("Untg_Rp_Pos")) %>% as.matrix() %>% log2()
+ints <- caseposall %>% select(starts_with("Untg_Rp")) %>% as.matrix() %>% log2()
 
-mod.adj3 <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + 
+clr1 <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + 
                                 QE_ENERGY + QE_ALC + L_School + Fasting_C + 
                                 strata(Match_Caseset), data = caseposall)
 
 # Apply model across subset
-mod.adj3 <- apply(posints, 2, res.adj3)
+mod.adj3 <- apply(ints, 2, clr1)
 
 res.adj3 <- map_df(mod.adj3, tidy, exponentiate = T, conf.int = T) %>% 
   filter(term == "x") %>% mutate(p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
   select(feature, everything(), -term) %>% mutate(feat.no = 1:length(mod.adj3))
 # No significant features after p-value adjustment
 
-
+# Format OR and CI
+tab.adj <- res.adj3 %>%
+  select(feature, estimate, conf.low, conf.high, everything()) %>%
+  mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("CI95", B1, conf.low, hyph, conf.high, B2, sep = "")
 
 
 # (c)	Possibly consider an analysis on the n=11 + n=17 where the cases are Hppos negative, but this will be weak.
 
 # This is concordant00 + discordant01 (28 pairs)
 casenegall <- bind_rows(concordant00, discordant01)
-posints <- casenegall %>% select(starts_with("Untg_Rp_Pos")) %>% as.matrix() %>% log2()
+ints <- casenegall %>% select(starts_with("Untg_Rp")) %>% as.matrix() %>% log2()
 
-mod.adj4 <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + 
+clr2 <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + 
                                 QE_ENERGY + QE_ALC + L_School + Fasting_C + 
                                 strata(Match_Caseset), data = casenegall)
 
 # Apply model across subset
-pos.adj <- apply(posints, 2, mod.adj4)
+pos.adj <- apply(ints, 2, clr2)
 
 res.adj <- map_df(pos.adj, tidy, exponentiate = T, conf.int = T) %>% 
   filter(term == "x") %>% mutate(p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
   select(feature, everything(), -term) %>% mutate(feat.no = 1:length(pos.adj))
 
-
+# Format OR and CI
+tab.adj <- res.adj3 %>%
+  select(feature, estimate, conf.low, conf.high, everything()) %>%
+  mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("CI95", B1, conf.low, hyph, conf.high, B2, sep = "")
 
 
 # (d)	Break matching and run unconditional analyses by Hppos, but with adjustment for country instead of by centre.
@@ -89,25 +106,51 @@ glm.hpp <- function(x) glm(Cncr_Caco_Stom ~ x + Sex + Country + #Use_Horm +
                                 data = posdat, family = "binomial")
 
 # Reset posdat from MetaGC_hp_subsets.R
-posints <- posdat %>% select(starts_with("Untg_Rp")) %>% as.matrix() %>% log2()
-
-features <- colnames(posints)
+ints <- posdat %>% select(starts_with("Untg_Rp")) %>% as.matrix() %>% log2()
 
 # Convert categorical variables to factors
 varlist <- c("Smoke_Stat", "Alc_Drinker", "Country", "L_School", "Fasting_C", "Use_Horm", "Sex", "HPPOS")
 posdat <- posdat %>% mutate(across((varlist), as.factor))
 
 # Apply model across subset
-pos.adj <- apply(posints, 2, glm.hpp)
+pos.adj <- apply(ints, 2, glm.hpp)
 
 #res.adj <- map_df(pos.adj, tidy, exponentiate = T, conf.int = T) %>% 
-res.adj <- map_df(pos.adj, tidy) %>% 
+res.adj <- map_df(pos.adj, tidy,  exponentiate = T) %>% 
   filter(term == "x") %>% 
   mutate(OR = exp(estimate), p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
   select(feature, everything(), -term) %>% mutate(feat.no = 1:length(pos.adj))
 
+# Format OR and CI
+tab.adj <- res.adj %>%
+  select(feature, estimate, everything()) %>%
+  #select(feature, estimate, conf.low, conf.high, everything()) %>%
+  mutate_at(vars(estimate:OR), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("CI95", B1, hyph, B2, sep = "")
 
 
 # (e)	In all sub-group analyses a, b, c and d above, also run analyses on the subset of n=230 who have missing data, 
-# possibly with heterogeneity analyses? [to do]
+# possibly with heterogeneity analyses?
+
+ints <- allmiss.hp %>% select(starts_with("Untg_Rp")) %>% as.matrix() %>% log2()
+
+clr <- function(x) clogit(Cncr_Caco_Stom ~ x + Bmi_C + Smoke_Stat + Pa_Total + QE_ENERGY + QE_ALC + 
+                                L_School + Fasting_C + strata(Match_Caseset), data = allmiss.hp)
+
+# Apply model across subset
+library(survival)
+mod.adj3 <- apply(ints, 2, clr)
+
+library(broom)
+res.adj3 <- map_df(mod.adj3, tidy, exponentiate = T, conf.int = T) %>% 
+  filter(term == "x") %>% mutate(p.adj = p.adjust(p.value, method = "fdr"), feature = features) %>%
+  select(feature, everything(), -term) %>% mutate(feat.no = 1:length(mod.adj3))
+
+# Format OR and CI
+tab.adj <- res.adj3 %>%
+  select(feature, estimate, conf.low, conf.high, everything()) %>%
+  mutate_at(vars(estimate:conf.high), ~ format(round(., digits = 2), nsmall = 2)) %>% 
+  mutate(B1 = " (", hyph = "-", B2 = ")") %>%
+  unite("CI95", B1, conf.low, hyph, conf.high, B2, sep = "")
   
